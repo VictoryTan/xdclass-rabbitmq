83324456b654494dc18acb3dca31feb99306465b
Tan Shiwei <853212308@qq.com>
2022-01-21 17:36:40 +0800

    Rabbitmq框架搭建

---
+++ src/test/java/net/xdclass/xdclasssp/XdclassSpApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xdclass.xdclasssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdclassSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
