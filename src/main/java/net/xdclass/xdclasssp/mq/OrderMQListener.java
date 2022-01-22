7b142c9a7cdeb71d6295c81347ba42982e780583
Tan Shiwei <853212308@qq.com>
2022-01-22 10:49:47 +0800

    mq消息可靠性demo

--- src/main/java/net/xdclass/xdclasssp/mq/OrderMQListener.java
+++ src/main/java/net/xdclass/xdclasssp/mq/OrderMQListener.java
@@ -1,21 +1,37 @@
 package net.xdclass.xdclasssp.mq;
 
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @RabbitListener(queues = "order_queue")
 public class OrderMQListener {
 
     @RabbitHandler
-    public void messageHandler(String body, Message message){
+    public void messageHandler(String body, Message message, Channel channel) throws IOException {
         long msgTag = message.getMessageProperties().getDeliveryTag();
         System.out.println("msgTag="+msgTag);
         System.out.println("message="+message.toString());
         System.out.println("body="+body);
+
+        //复杂业务逻辑
+
+
+        //告诉broker，消息已经被确认
+        channel.basicAck(msgTag,false);
+
+        //告诉broker，消息拒绝确认
+        //channel.basicNack(msgTag,false,true);
+
+        //channel.basicReject(msgTag,true);
+
+
     }
 
 }
--- src/test/java/net/xdclass/xdclasssp/XdclassSpApplicationTests.java
+++ src/test/java/net/xdclass/xdclasssp/XdclassSpApplicationTests.java
@@ -2,6 +2,8 @@ package net.xdclass.xdclasssp;
 
 import net.xdclass.xdclasssp.config.RabbitMqConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.ReturnedMessage;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,14 +12,81 @@ import org.springframework.boot.test.context.SpringBootTest;
 class XdclassSpApplicationTests {
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private RabbitTemplate template;
+
 
     @Test
     void testSend() {
-        for (int i = 0; i < 5; i++) {
 
-            rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, "order.new", "你好"+i);
-        }
+        //for(int i=0;i<5;i++){
+        template.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, "order.new","新订单777");
+        //}
+
+    }
+
+
+    /**
+     * 生产者到交换机可靠性投递测试
+     */
+    @Test
+    void testConfirmCallback(){
+
+        template.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+
+            /**
+             * @param correlationData 配置
+             * @param ack 交换机是否收到消息，true是成功，false是失败
+             * @param cause 失败的原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+
+                System.out.println("ConfirmCallback======>");
+                System.out.println("correlationData======>correlationData="+correlationData);
+                System.out.println("ack======>ack="+ack);
+                System.out.println("cause======>cause="+cause);
+
+                if(ack){
+                    System.out.println("发送成功");
+                    //更新数据库 消息的状态为成功  TODO
+                }else {
+                    System.out.println("发送失败，记录到日志或者数据库");
+                    //更新数据库 消息的状态为失败  TODO
+                }
+
+            }
+        });
+
+        //数据库新增一个消息记录，状态是发送  TODO
+
+        //发送消息
+        //template.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "order.new","新订单");
+        //模拟异常
+        template.convertAndSend(RabbitMqConfig.EXCHANGE_NAME+" xdclass", "order.new","新订单");
+
+    }
+
+
+    /**
+     * 交换机到队列可靠性投递测试
+     */
+    @Test
+    void testReturnCallback(){
+
+        template.setReturnsCallback(new RabbitTemplate.ReturnsCallback() {
+
+            @Override
+            public void returnedMessage(ReturnedMessage returned) {
+                int code = returned.getReplyCode();
+                System.out.println("code="+code);
+                System.out.println("returned="+returned.toString());
+
+            }
+        });
+
+        //template.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "order.new","新订单ReturnsCallback");
+        //模拟异常
+        template.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, "xdclass.order.new","新订单ReturnsCallback");
 
     }
 
